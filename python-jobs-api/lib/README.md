00f43fb5a1f83f47d29f99978bb8b981c81bc448
Valentin Podkamennyi <valentin@dtm.io>
2019-05-05 18:54:49 -0700

    Added solution for Problem 1 for the Python coding challenge.

---
+++ python-jobs-api/lib/README.md
@@ -0,0 +1,27 @@
+# Problem #1
+
+Make a job processing system in which jobs can have dependencies on other jobs. A user specifies the job they would like to run with a text file that looks like this:
+```bash
+# id identifying job in rest of file.
+job_id job1
+# program and arguments. run the program through the bash shell.
+program cat /tmp/file2 /tmp/file3 /tmp/file4
+# jobs depended on.
+parent_job_ids job2 job3 job4
+job_id job2
+program echo hi > /tmp/file2
+job_id job3
+program echo bye > /tmp/file3
+job_id job4
+program cat /tmp/file2 > /tmp/file4; echo again >> /tmp/file4
+parent_job_ids job2
+```
+Write a program in Python which takes this input and runs the jobs to completion with these conditions:
+
+- Before a job can run, the jobs it depends on must have finished.
+- Run jobs in parallel when possible. The user specifies the maximum number of
+simultaneous running jobs as an argument to the program.
+
+Also
+- If you need external libraries, include their source code.
+- Include the steps to build your program.
---
+++ python-jobs-api/lib/jobs.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# http://google.github.io/styleguide/pyguide.html
+"""Simple job processing system."""
+
+import os
+import subprocess
+import sys
+import threading
+
+SESSION = {'running': 0}
+
+
+def _parse(filepath):
+    """Parses input file.
+
+    Args:
+        filepath: The file path to parse.
+
+    Returns:
+        A dict with jobs found in file.
+    """
+    jobs = {}
+    job_id = None
+
+    with open(filepath, 'r') as fp:
+        for line in fp:
+            line = line.strip()
+            if line[:6] == 'job_id':
+                job_id = line[7:]
+                jobs[job_id] = {'job_id': job_id, 'complete': False}
+            elif line[:7] == 'program':
+                jobs[job_id]['program'] = line[8:]
+            elif line[:14] == 'parent_job_ids':
+                jobs[job_id]['parents'] = line[15:].split(' ')
+
+    return jobs
+
+
+def _exec(job):
+    """Executes job command.
+
+    Args:
+        job: The job object with the command to execute.
+
+    See:
+        https://docs.python.org/3/library/os.html#os.system
+    """
+    os.system(job['program'])
+    job['complete'] = True
+    SESSION['running'] -= 1
+    _run()
+
+
+def _run():
+    """Runs all jobs."""
+    SESSION['locker'].acquire()
+    jobs = SESSION['jobs']
+
+    for job_id in jobs:
+        job = jobs[job_id]
+        if not job['complete']:
+            parents = job.get('parents') or []
+            if not [parent_id for parent_id in parents if \
+               not jobs[parent_id]['complete']]:
+                thread = threading.Thread(target=_exec, args=(job, ))
+                thread.start()
+                SESSION['running'] += 1
+                if SESSION['running'] >= SESSION['threads']:
+                    break
+
+    SESSION['locker'].release()
+
+
+def main():
+    filepath = None
+    threads = None
+
+    if len(sys.argv) > 1:
+        filepath = sys.argv[-1]
+        if len(sys.argv) > 3 and sys.argv[1] == '-t' and sys.argv[2].isdigit():
+            threads = int(sys.argv[2])
+
+    if filepath and os.path.isfile(filepath):
+        # https://docs.python.org/3/library/threading.html#threading.Lock
+        SESSION['locker'] = threading.Lock()
+        SESSION['threads'] = threads or 1
+        SESSION['jobs'] = _parse(filepath)
+        _run()
+    else:
+        print('Usage: %s [-t #] file' % __file__)
+
+
+if __name__ == "__main__":
+    main()
